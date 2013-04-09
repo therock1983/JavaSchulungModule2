7fc14d2f6e116a385c8c0ee654e5aceb31d68242
Thomas Gödl <therock1983@gmail.com>
2013-04-09 17:32:32 +0200

    git tryout

---
+++ ShortestPath/src/at/edu/hti/shortestpath/DummyClass.java
@@ -0,0 +1,31 @@
+
+/** 
+ * Copyright 2013 SSI Schaefer PEEM GmbH. All Rights reserved. 
+ * <br /> <br />
+ * 
+ * $Id$
+ * <br /> <br />
+ *
+ */
+
+package at.edu.hti.shortestpath;
+
+
+/**
+ * This is the class header. The first sentence (ending with "."+SPACE) is important,
+ * because it is used summary in the package overview pages.<br />
+ * <br />
+ *
+ *
+ * @author  goedl
+ * @version $Revision$
+ */
+
+public class DummyClass {
+
+}
+
+
+//---------------------------- Revision History ----------------------------
+//$Log$
+//
